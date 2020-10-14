a7c892cdb8b3b6e4901faa16992d67498468116f
natyudon <61405828+natyudon@users.noreply.github.com>
2020-10-14 14:38:23 +0900

    activeHash導入

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -2,6 +2,7 @@ class CaloriesController < ApplicationController
   def index
   end
   def new
+    @menu_data = MenuDatum.all
   end
   def tesma_about
     if params[:data]
---
+++ app/models/menu_datum.rb
@@ -0,0 +1,22 @@
+class MenuDatum < ActiveHash::Base
+  self.data = [
+    {id: 1, name: 'バラ（カルビ）',calory: 426, calory2: 371},
+    {id: 2, name: 'リブロース', calory: 409, calory2: 231},
+    {id: 3, name: 'サーロイン',calory: 334, calory2: 298},
+    {id: 4, name: 'かたロース', calory: 318, calory2: 240},
+    {id: 5, name: 'かた',calory: 257, calory2: 180},
+    {id: 6, name: 'ランプ', calory: 248, calory2: 234},
+    {id: 7, name: 'もも',calory: 209, calory2: 165},
+    {id: 8, name: 'ヒレ', calory: 195, calory2: 133},
+    {id: 9, name: 'テール（尾）',calory: 492, calory2: 492},
+    {id: 10, name: 'タン', calory: 356, calory2: 356},
+    {id: 11, name: '小腸',calory: 287, calory2: 287},
+    {id: 12, name: '大腸', calory: 162, calory2: 162},
+    {id: 13, name: 'ハツ',calory: 142, calory2: 142},
+    {id: 14, name: 'レバー', calory: 132, calory2: 132},
+    {id: 15, name: 'じん臓',calory: 131, calory2: 131},
+    {id: 16, name: '直腸', calory: 115, calory2: 115},
+    {id: 17, name: 'せんまい', calory: 62, calory2: 62},
+    {id: 18, name: 'はらみ', calory: 301, calory2: 301},
+  ]
+end
