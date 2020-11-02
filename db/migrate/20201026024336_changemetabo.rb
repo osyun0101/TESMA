08604e2862a5a4d179f7c6a06806eb294511a7da
natyudon <61405828+natyudon@users.noreply.github.com>
2020-11-02 17:18:51 +0900

    migration整理

--- db/migrate/20201026024336_changemetabo.rb
+++
@@ -1,5 +0,0 @@
-class Changemetabo < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :metabolism, :string, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_071621) do
+ActiveRecord::Schema.define(version: 2020_11_02_081634) do
 
   create_table "calories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "calory", null: false
